48d72d6bf751e0c04d3229b688b01b1c09dcc488
harshitbhomawat <harshitbhomawat@gmail.com>
2022-09-01 21:50:34 +0530

    Attacking Table Ready

---
+++ models/attacking.js
@@ -0,0 +1,30 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Attacking extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Attacking.init({
+    player_name: DataTypes.STRING,
+    club: DataTypes.STRING,
+    position: DataTypes.STRING,
+    asists: DataTypes.INTEGER,
+    corner_taken: DataTypes.INTEGER,
+    offsides: DataTypes.INTEGER,
+    dribbles: DataTypes.INTEGER,
+    match_played: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Attacking',
+  });
+  return Attacking;
+};
--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { Player } = require('../models');
+const { Player, Attacking } = require('../models');
+const csvtojson = require('csvtojson');
+const attacking = require('../models/attacking');
+// const dataset = require('../datasets');
 
 router.get("/",(req,res,next)=>{
     Player.findAll().then(data => res.json(data)).catch(next);
@@ -23,5 +26,41 @@ router.post("/",(req,res)=>{
         }
     });
 });
+
+router.get("/table",(req,res,next)=>{
+    Attacking.findAll().then(data => res.json(data)).catch(next);
+});
+
+router.post("/table",(req,res)=>{
+    const filename = 'E:/Ringover App/Ringover-app/datasets/attacking.csv';
+    csvtojson().fromFile(filename).then(source=>{
+        
+        for (var i = 0; i < source.length; i++) {
+            var player_name = source[i]["player_name"],
+            club= source[i]["club"],
+            position= source[i]["position"],
+            assists= source[i]["assists"],
+            corner_taken= source[i]["corner_taken"],
+            offsides= source[i]["offsides"],
+            dribbles= source[i]["dribbles"],
+            match_played= source[i]["match_played"];
+            Attacking.create({
+                player_name : player_name,
+                club : club,
+                position : position,
+                assists : assists,
+                corner_taken : corner_taken,
+                offsides : offsides,
+                dribbles : dribbles,
+                match_played : match_played
+            }).then(data => {
+                console.log(data);
+            }).catch(error => {
+                console.log(error);
+            })
+        }
+    })
+    res.sendStatus(200);
+})
 
 module.exports = router;
